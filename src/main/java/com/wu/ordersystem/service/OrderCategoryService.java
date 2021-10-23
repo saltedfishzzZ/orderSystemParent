274026648c7d066e2c5d088afcef89543a251a04
wujianxin <wujianxin@corp.51xiaodou.com>
2021-10-23 14:13:16 +0800

    添加类别批量删除接口

--- src/main/java/com/wu/ordersystem/service/OrderCategoryService.java
+++ src/main/java/com/wu/ordersystem/service/OrderCategoryService.java
@@ -4,6 +4,8 @@ import com.wu.ordersystem.pojo.domain.OrderCategory;
 import com.wu.ordersystem.pojo.dto.OrderCategoryDTO;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+
 /**
  * @author saltedfishzzZ
  * @date 2021-10-10
@@ -14,4 +16,6 @@ public interface OrderCategoryService {
     Page<OrderCategory> listCategory(Long id, Integer pageNo, Integer pageSize);
 
     boolean updateById(Long id, OrderCategoryDTO orderCategoryDTO);
+
+    void batchDeleteByIdList(List<Long> idList);
 }
