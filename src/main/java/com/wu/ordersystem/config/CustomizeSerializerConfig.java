6be03ecc6f1850df0c5579ea9e67d24c22207b37
saltedfishzzZ <saltedfishzzZ@qq.com>
2021-11-02 11:10:03 +0800

    修改配置文件 添加jackson反序列化bean

--- src/main/java/com/wu/ordersystem/config/CustomizeSerializerConfig.java
+++ src/main/java/com/wu/ordersystem/config/CustomizeSerializerConfig.java
@@ -1,5 +1,10 @@
 package com.wu.ordersystem.config;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +12,8 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -20,14 +27,24 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class CustomizeSerializerConfig {
 
+    @Value("${spring.jackson.datetime-format}")
+    private String dateTimePattern;
+
     @Value("${spring.jackson.date-format}")
     private String datePattern;
 
-    private static final String timePattern = "HH:mm:ss";
+    @Value("${spring.jackson.time-format}")
+    private String timePattern;
+//    private static final String timePattern = "HH:mm:ss";
 
     @Bean
     public LocalDateTimeSerializer localDateTimeSerializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(datePattern));
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimePattern));
+    }
+
+    @Bean
+    public LocalDateSerializer localDateSerializer() {
+        return new LocalDateSerializer(DateTimeFormatter.ofPattern(datePattern));
     }
 
     @Bean
@@ -35,11 +52,45 @@ public class CustomizeSerializerConfig {
         return new LocalTimeSerializer(DateTimeFormatter.ofPattern(timePattern));
     }
 
+    @Bean
+    public JsonDeserializer<LocalDateTime> localDateTimeJsonDeserializer() {
+        return new JsonDeserializer<LocalDateTime>() {
+            @Override
+            public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                return LocalDateTime.parse(jsonParser.getText(), DateTimeFormatter.ofPattern(dateTimePattern));
+            }
+        };
+    }
+
+    @Bean
+    public JsonDeserializer<LocalDate> localDateJsonDeserializer() {
+        return new JsonDeserializer<LocalDate>() {
+            @Override
+            public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                return LocalDate.parse(jsonParser.getText(), DateTimeFormatter.ofPattern(datePattern));
+            }
+        };
+    }
+
+    @Bean
+    public JsonDeserializer<LocalTime> localTimeJsonDeserializer() {
+        return new JsonDeserializer<LocalTime>() {
+            @Override
+            public LocalTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                return LocalTime.parse(jsonParser.getText(), DateTimeFormatter.ofPattern(timePattern));
+            }
+        };
+    }
+
     @Bean("objectMapper")
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> {
             builder.serializerByType(LocalDateTime.class, localDateTimeSerializer());
+            builder.serializerByType(LocalDate.class, localDateSerializer());
             builder.serializerByType(LocalTime.class, localTimeSerializer());
+            builder.deserializerByType(LocalDateTime.class, localDateTimeJsonDeserializer());
+            builder.deserializerByType(LocalDate.class, localDateJsonDeserializer());
+            builder.deserializerByType(LocalTime.class, localTimeJsonDeserializer());
         };
     }
 }
